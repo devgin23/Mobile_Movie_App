4639138a11f9a70438af820e2ba3599ea0b8d46e
Gin <devgin23@gmail.com>
2025-05-26 23:56:01 +0900

    appwrite를 사용한 검색 이력 데이터 생성 로직, SDK 버전 차이로 예외 던지고 있음

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,13 +3,14 @@ import SearchBar from '@/components/SearchBar'
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api'
+import { updateSearchCount } from '@/services/appwrite'
 import useFetch from '@/services/useFetch'
 import { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 
 const search = () => {
 
-  const [searchQuery, setSearchQeury] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
 
   const { 
     data: movies, 
@@ -22,10 +23,15 @@ const search = () => {
   }), false)
 
   useEffect(()=> {
+
     // 타자 치는게 멈추고 나서 몇 초가 흐르면 그제서야 호출하기.
     const timeoutId = setTimeout(async () => {
       if(searchQuery.trim()) {
         await loadMovies();
+
+        if(movies?.length > 0 && movies?.[0]) 
+          await updateSearchCount(searchQuery, movies[0]);
+
       } else {
         reset();
       }
@@ -61,7 +67,7 @@ const search = () => {
               <SearchBar 
                 placeholder='Search movies ... ' 
                 value={searchQuery}
-                onChangeText={(text: string) => setSearchQeury(text)}
+                onChangeText={(text: string) => setSearchQuery(text)}
               />
             </View>
 
